5f43d372592a84daabae0dfbd25a498c8e62d365
@Fresh Programing <93295405+FreshPhp@users.noreply.github.com>
2022-12-13 11:01:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-const wa = require('@open-wa/wa-automate')
-const config = require('./config')
+const wa = require('@open-wa/wa-automate');
+const config = require('./config');
 
-// A função que o faz carregar as mensagens
 function start(fresh) {
 	fresh.onMessage(async message => await config(fresh, message))
-} // Função que analisa o recebimento de mensagens
+};
 
 // Cria o cliente de inicialização
 wa.create().then(fresh => start(fresh))
